c26251a03636fac3739ece9548577c104abb3327
goeun <20210031@duksung.ac.kr>
2026-06-30 11:56:17 +0900

    version control5_B

--- chapter01/Version3.java
+++ chapter01/Version3.java
@@ -3,5 +3,7 @@ package chapter01;
 public class Version3 {
     public Version3() {
         System.out.println("A");
+        System.out.println("B");
+        System.out.println("C");
     }    
 }
